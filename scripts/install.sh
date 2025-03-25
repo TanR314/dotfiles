bc7ec89783d0b3df1ec89a3fc47c5641e4aa0aea
Jawad Hameem Chowdhury <jawadhc314@gmail.com>
2025-03-25 13:20:06 +0600

    Modified some scripts

---
+++ scripts/install.sh
@@ -0,0 +1,33 @@
+sudo pacman -Sy
+
+
+
+# Hyprland
+sudo pacman -S --needed xdg-desktop-portal-hyprland hyprland wofi uwsm grim slurp
+sudo pacman -S --needed swww brightnessctl fuzzel
+
+
+
+# Ignis
+yay -S --needed ignis-git dart-sass sassc
+
+
+
+# Editors
+sudo pacman -S --needed vscodium neovim tree-sitter-cli nodejs wl-clipboard
+
+
+
+# Apps
+sudo pacman -S --needed telegram-desktop ayugram-desktop zathura zathura-pdf-mupdf
+sudo pacman -S --needed ghostty zen-browser-bin
+
+
+
+# Bluetooth Setup
+sudo pacman -S --needed bluez bluez-utils bluez-deprecated-tools blueman
+
+
+
+# Dotfiles Manager
+sudo pacman -S --needed stow
---
+++ scripts/tlmgr-setup.sh
@@ -0,0 +1,9 @@
+tlmgr install latexmk xetex luatex tcolorbox tikzfill haranoaji fontawesome fontawesome5 mdframed zref needspace pdfcol enumitem epigraph nextpage latexbangla titlesec chngcntr memoir xpatch booktabs mdwtools thmtools listings asymptote
+# haranoaji ( required for "luatexja" a package alternative to fontspec in xelatex for lualatex)
+# zref ( for mdframed )
+# needspace (for mdframed)
+# enumitem ( For Evan-Chen Otis-template )
+# epigraph ( For Evan-Chen Otis-template )
+# nextpage ( For Evan-Chen Otis-template )
+# titlesec ( For latexbangla )
+# chngcntr ( For latexbangla, If I remember correctly )
