e8ef1a2f896d09aa1b883afcad6cecb488383799
TanR <jawadhc314@gmail.com>
2025-07-30 02:18:29 +0600

    caelestia replaced ignis

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,7 @@
 source ~/.config/fish/cachyos-config.fish
+set -gx MANPATH "$MANPATH:/home/tanr/texlive/2025/texmf-dist/doc/man"
+set -gx INFOPATH "$INFOPATH:/home/tanr/texlive/2025/texmf-dist/doc/info"
+
 
 # overwrite greeting
 # potentially disabling fastfetch
