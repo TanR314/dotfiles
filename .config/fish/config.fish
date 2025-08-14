fef2152a9ed48a20f8a9c5a14bd0482a392fa62e
jawad <jawadhc314@gmail.com>
2025-08-15 01:15:59 +0600

    updated fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,4 @@
-source ~/.config/fish/cachyos-config.fish
-set -gx MANPATH "$MANPATH:/home/tanr/texlive/2025/texmf-dist/doc/man"
-set -gx INFOPATH "$INFOPATH:/home/tanr/texlive/2025/texmf-dist/doc/info"
-
+# source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 # overwrite greeting
 # potentially disabling fastfetch
